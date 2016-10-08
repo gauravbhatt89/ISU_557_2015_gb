4c8b69118d2f062c510fcf3a22661a0efbb2e6cc
Gaurav Bhatt <bhatt.gaurav89@gmail.com>
2016-10-07 20:27:16 -0500

    Committing HW3

---
+++ Homework_3/main_lighting.cpp
@@ -0,0 +1,133 @@
+//
+//  main.cpp
+//  OpenGL4Test
+//  Created by Dipit Malhotra on 10/7/16.
+
+
+// stl include
+#include <iostream>
+#include <string>
+
+// GLEW include
+#include <GL/glew.h>
+
+// GLM include files
+#define GLM_FORCE_INLINE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+
+
+// glfw includes
+#include <GLFW/glfw3.h>
+
+
+// include local files
+#include "controls.h"
+#include "HCI557Common.h"
+#include "CoordSystem.h"
+//#include "GLSphere.h"
+#include "GLSphere.h"
+//#include "GLSphereRed.h"
+//#include "GLSphereGreen.h"
+//#include "GLSphereYellow.h"
+
+
+using namespace std;
+
+
+// The handle to the window object
+GLFWwindow*         window;
+
+// Define some of the global variables we're using for this sample
+GLuint program;
+
+/* A trackball to move and rotate the camera view */
+extern Trackball trackball;
+
+
+
+int main(int argc, const char * argv[])
+{
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//// Init glfw, create a window, and init glew
+
+	// Init the GLFW Window
+	window = initWindow();
+
+
+	// Init the glew api
+	initGlew();
+
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//// Create some models
+
+	// coordinate system
+	CoordSystem* cs = new CoordSystem(40.0);
+
+
+	GLSphere* sphere1 = new GLSphere(-25.0, 0.0, 0.0, 3.0, 40, 20);
+	GLSphere* sphere2 = new GLSphere(-10.0, 0.0, 0.0, 3.0, 40, 20);
+	GLSphere* sphere3 = new GLSphere(5.0, 0.0, 0.0, 3.0, 40, 20);
+	GLSphere* sphere4 = new GLSphere(20.0, 0.0, 0.0, 3.0, 40, 20);
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//// Main render loop
+
+	// Set up our green background color
+	static const GLfloat clear_color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	static const GLfloat clear_depth[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+	// This sets the camera to a new location
+	// the first parameter is the eye position, the second the center location, and the third the up vector.
+	SetViewAsLookAt(glm::vec3(-3.0f, 0.0f, 45.0f), glm::vec3(-3.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+
+	// Enable depth test
+	// ignore this line, it allows us to keep the distance value after we proejct each object to a 2d canvas.
+	glEnable(GL_DEPTH_TEST);
+
+	//sphere->enableNormalVectorRenderer();
+
+	// This is our render loop. As long as our window remains open (ESC is not pressed), we'll continue to render things.
+	while (!glfwWindowShouldClose(window))
+	{
+
+		// Clear the entire buffer with our green color (sets the background to be green).
+		glClearBufferfv(GL_COLOR, 0, clear_color);
+		glClearBufferfv(GL_DEPTH, 0, clear_depth);
+
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//// This renders the objects
+
+		// Set the trackball locatiom
+		SetTrackballLocation(trackball.getRotationMatrix());
+
+		// draw the objects
+		cs->draw();
+
+		sphere1->draw();
+		sphere2->draw();
+		sphere3->draw();
+		sphere4->draw();
+		//// This renders the objects
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+		// Swap the buffers so that what we drew will appear on the screen.
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+
+	delete cs;
+
+
+}
